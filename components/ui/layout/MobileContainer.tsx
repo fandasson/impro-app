6fa490f38227c3bc56492c75e3e2aac608930174
František Pác <5476595+fandasson@users.noreply.github.com>
2024-02-11 11:50:07 +0100

    feat(audience): add `MobileContainer`

---
+++ components/ui/layout/MobileContainer.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react";
+
+import { cn } from "@/utils/styling.utils";
+
+type Props = PropsWithChildren & React.HTMLAttributes<"div">;
+export const MobileContainer = ({ children, className }: Props) => {
+    return <main className={cn("container flex h-screen flex-col gap-6 p-6", className)}>{children}</main>;
+};
