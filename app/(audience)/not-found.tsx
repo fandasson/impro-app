136725e3464a5edaf0799a423a4ea79b196c0371
František Pác <5476595+fandasson@users.noreply.github.com>
2024-02-11 11:49:54 +0100

    feat(audience): add layout and not-found page

---
+++ app/(audience)/not-found.tsx
@@ -0,0 +1,5 @@
+import { CenteredContainer } from "@/components/ui/layout/CenteredContainer";
+
+export default async function NotFound() {
+    return <CenteredContainer>Nevíme, nemáme 😳</CenteredContainer>;
+}
