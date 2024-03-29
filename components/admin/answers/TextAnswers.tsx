290d18bdc27f403ffb003624b9fe318ed87441dd
František Pác <5476595+fandasson@users.noreply.github.com>
2024-03-29 20:54:43 +0100

    feat(style): layout answers as list instead of grid

--- components/admin/answers/TextAnswers.tsx
+++ components/admin/answers/TextAnswers.tsx
@@ -49,13 +49,13 @@ export const TextAnswers = ({ questionId, answers: initialAnswers }: Props) => {
     }, [setAnswers, answers, questionId]);
 
     return (
-        <div className={"flex flex-col items-center"}>
-            <div className={"mb-8 flex flex-wrap gap-4"}>
+        <div className={"flex flex-col"}>
+            <div className={"mb-8 grid grid-cols-1 gap-4"}>
                 {answers.map((answer) => {
                     const selected = selectedAnswers.includes(answer.id);
                     return (
                         <Badge
-                            className={"text-md po cursor-pointer px-3.5 py-2"}
+                            className={"text-md cursor-pointer px-3.5 py-2"}
                             key={answer.id}
                             variant={`${selected ? "default" : "outline"}`}
                             onClick={() => toggleSelect(answer.id)}
--- components/ui/Badge.tsx
+++ components/ui/Badge.tsx
@@ -4,14 +4,14 @@ import * as React from "react";
 import { cn } from "@/utils/styling.utils";
 
 const badgeVariants = cva(
-    "inline-flex items-center rounded-full border px-2.5 py-1 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+    "inline-flex items-center rounded border px-2.5 py-1 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
     {
         variants: {
             variant: {
                 default: "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
                 secondary: "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
                 destructive: "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-                outline: "text-foreground",
+                outline: "text-foreground bg-background-light",
             },
         },
         defaultVariants: {
