160db1a74b46354283e6b6c765b6e514457db3f3
František Pác <5476595+fandasson@users.noreply.github.com>
2024-04-05 14:39:34 +0200

    feat(audience): realtime listen on question change

--- components/audience/AudienceQuestionDetail.tsx
+++ components/audience/AudienceQuestionDetail.tsx
@@ -5,6 +5,7 @@ import { QuestionHeadline } from "@/components/audience/QuestionHeadline";
 import { Timer } from "@/components/audience/Timer";
 import { MatchQuestionAnswers } from "@/components/audience/answers/MatchAnswer";
 import { QuestionAnswers } from "@/components/audience/answers/QuestionAnswers";
+import { createClient } from "@/utils/supabase/client";
 import { Tables } from "@/utils/supabase/entity.types";
 
 type Props = {
@@ -16,6 +17,28 @@ export const AudienceQuestionDetail = ({ performanceId }: Props) => {
     useEffect(() => {
         fetchActiveQuestion(performanceId).then((response) => setQuestion(response.data));
     }, [performanceId]);
+
+    // @ts-ignore
+    useEffect(() => {
+        const supabase = createClient();
+        const channel = supabase
+            .channel("question-channel")
+            .on<Tables<"questions">>(
+                "postgres_changes",
+                {
+                    event: "UPDATE",
+                    schema: "public",
+                    table: "questions",
+                    filter: `id=eq.${question?.id || -1}`,
+                },
+                (payload) => {
+                    setQuestion(payload.new);
+                },
+            )
+            .subscribe();
+
+        return () => supabase.removeChannel(channel);
+    }, [question]);
 
     if (!question) {
         return null;
