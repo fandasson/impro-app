31e383e4e595155b15a58f563f50c1c222cb8e12
František Pác <5476595+fandasson@users.noreply.github.com>
2024-05-22 21:42:53 +0200

    feat(admin): show audience visibility on questions list

--- components/admin/questions/QuestionItem.tsx
+++ components/admin/questions/QuestionItem.tsx
@@ -1,4 +1,4 @@
-import { Eye, EyeOff, Pencil, UserCheck } from "lucide-react";
+import { Eye, EyeOff, HelpCircle, Pencil, ProjectorIcon, SmartphoneIcon } from "lucide-react";
 import Link from "next/link";
 import { ReactNode } from "react";
 
@@ -6,11 +6,12 @@ import { QuestionWithPool } from "@/api/types.api";
 
 type Props = QuestionWithPool;
 export const QuestionItem = (props: Props) => {
-    const { id, name, state, questions_pool: pool } = props;
+    const { id, name, state, audience_visibility, questions_pool: pool } = props;
 
     let stateIcon: ReactNode | null = null;
     switch (state) {
         case "draft":
+        case "answered":
             stateIcon = <EyeOff />;
             break;
         case "active":
@@ -19,16 +20,33 @@ export const QuestionItem = (props: Props) => {
         case "locked":
             stateIcon = <Eye className={"stroke-destructive"} />;
             break;
-        case "answered":
-            stateIcon = <UserCheck />;
+    }
+
+    let audienceVisibilityIcon: ReactNode | null = null;
+    switch (audience_visibility) {
+        case "hidden":
+            audienceVisibilityIcon = <EyeOff />;
+            break;
+        case "question":
+            audienceVisibilityIcon = <HelpCircle className={"stroke-destructive"} />;
+            break;
+        case "results":
+            audienceVisibilityIcon = <Eye className={"stroke-destructive"} />;
             break;
     }
 
     return (
-        <Link href={`/admin/questions/${id}/view`} className={"flex justify-between rounded border px-3.5 py-3"}>
-            <strong>{name}</strong>
+        <Link href={`/admin/questions/${id}/view`} className={"grid grid-cols-7 gap-2 rounded border px-3.5 py-3"}>
+            <strong className={"col-span-4"}>{name}</strong>
             <span>{pool?.name}</span>
-            {stateIcon}
+            <div className={"flex items-center justify-end"}>
+                <SmartphoneIcon size={28} className={"mr-4"} />
+                {stateIcon}
+            </div>
+            <div className={"flex items-center justify-end"}>
+                <ProjectorIcon size={28} className={"mr-4"} />
+                {audienceVisibilityIcon}
+            </div>
         </Link>
     );
 };
