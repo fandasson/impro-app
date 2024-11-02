f838b705993ad3cfda50d0943b4ca2edc9c7229a
František Pác <5476595+fandasson@users.noreply.github.com>
2024-11-02 15:02:34 +0100

    fix(user): fix scrolling with tall question

--- components/ui/layout/MobileContainer.tsx
+++ components/ui/layout/MobileContainer.tsx
@@ -4,7 +4,5 @@ import { cn } from "@/utils/styling.utils";
 
 type Props = PropsWithChildren & React.HTMLAttributes<"div">;
 export const MobileContainer = ({ children, className }: Props) => {
-    return (
-        <main className={cn("container flex h-svh flex-col gap-6 overflow-y-hidden p-6", className)}>{children}</main>
-    );
+    return <main className={cn("container flex min-h-svh flex-col gap-6 p-6", className)}>{children}</main>;
 };
