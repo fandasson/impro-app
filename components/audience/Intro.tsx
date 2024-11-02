ee31ab9b982caf2ecf8fd692109fd222f65b67b9
František Pác <5476595+fandasson@users.noreply.github.com>
2024-11-02 15:33:50 +0100

    ui(auidence): impro text visibility

--- components/audience/Intro.tsx
+++ components/audience/Intro.tsx
@@ -6,17 +6,13 @@ type Props = {
     performance: Tables<"performances">;
 };
 export const Intro = ({ performance }: Props) => {
-    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/${performance.url_slug}`;
+    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/${performance.url_slug}`.replace("https://", "");
     return (
         <div className={"flex flex-col items-center gap-8"}>
             <h1 className={"text-center text-xl"}>{performance.name}</h1>
-            <h2 className={"text-center text-2xl font-bold"}>
-                Chcete ovlivnit představení?
-                <br />
-                Skenujte 👇
-            </h2>
-            <QRCodeSVG value={url} bgColor={"#000000"} fgColor={"#FFFFFF"} size={256} />
-            <pre className={"font-mono text-xl"}>{url}</pre>
+            <h2 className={"text-center text-2xl font-bold"}>Zapojte se do představení</h2>
+            <QRCodeSVG value={url} bgColor={"#000000"} fgColor={"#FFFFFF"} size={330} />
+            <pre className={"font-mono text-4xl"}>{url}</pre>
         </div>
     );
 };
