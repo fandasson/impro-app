c1e3369bb28763a45bbc1329349fbc145bd29696
František Pác <5476595+fandasson@users.noreply.github.com>
2025-05-12 18:23:01 +0200

    chore: modify next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "www.improvariace.cz",
+                pathname: "/media/**",
                 port: "",
             },
         ],
