fb232f58961a62e6100cf6430b79d34a9510e4fb
František Pác <5476595+fandasson@users.noreply.github.com>
2025-05-12 22:54:51 +0200

    refactor: have upcoming page as a separate and not as not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,19 @@
-import { getUpcomingPerformances } from "@/api/web.api";
 import { Paragraph } from "@/components/ui/Paragraph";
-import { PerformanceList } from "@/components/ui/PerformanceList";
 import { MobileContainer } from "@/components/ui/layout/MobileContainer";
 import { SharingActions } from "@/components/users/SharingActions";
 
 export default async function NotFound() {
-    const upcomingPerformances = await getUpcomingPerformances();
     return (
         <MobileContainer>
             <div className="flex flex-grow flex-col">
-                {upcomingPerformances.length === 0 && (
-                    <div className="flex flex-grow flex-col justify-center text-center">
-                        <Paragraph>Máme prázdniny ☀️🌴🏖️</Paragraph>
-                    </div>
-                )}
-                {upcomingPerformances.length > 0 && (
-                    <div>
-                        <div className="py-2 pb-4 text-center">
-                            <h1 className="text-xl font-bold">Nejbližší představení</h1>
-                        </div>
-                        <PerformanceList performances={upcomingPerformances} />
-                    </div>
-                )}
+                <div className="flex flex-grow flex-col justify-center text-center">
+                    <Paragraph>No to je trapas!</Paragraph>
+                    <Paragraph>Žádný nápad, co vám zobrazit. Nic.</Paragraph>
+                    <Paragraph>To se nikdy nemělo stát.</Paragraph>
+                    <Paragraph>Ale co už.</Paragraph>
+                    <Paragraph>Ale zůstane to mezi námi že jo?</Paragraph>
+                    <Paragraph className={"mt-2"}>mrk mrk</Paragraph>
+                </div>
             </div>
             <SharingActions />
         </MobileContainer>
