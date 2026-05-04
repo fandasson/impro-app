4560ba020cb54c44d4c409b845340ed9d5db51ff
František Pác <5476595+fandasson@users.noreply.github.com>
2026-05-04 23:59:22 +0200

    chore: clean-up code, removing duplicates

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { fetchPerformances } from "@/api/performances.api";
+import { PerformanceList } from "@/components/admin/performances/PerformanceList";
 import { Button } from "@/components/ui/Button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/Table";
-import { formatDate } from "@/utils/date.utils";
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Performances() {
@@ -31,30 +30,7 @@ export default async function Performances() {
                     <Link href="/admin/performances/new">Vytvořit představení</Link>
                 </Button>
             </div>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Jméno</TableHead>
-                        <TableHead>Datum</TableHead>
-                        <TableHead className="w-[100px]">Stav</TableHead>
-                        <TableHead />
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {performances.map((performance) => (
-                        <TableRow key={performance.id}>
-                            <TableCell>{performance.name}</TableCell>
-                            <TableCell>{formatDate(performance.date)}</TableCell>
-                            <TableCell>{performance.state}</TableCell>
-                            <TableCell>
-                                <Button asChild>
-                                    <Link href={`/admin/performances/${performance.id}`}>Otevřít</Link>
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            <PerformanceList performances={performances} />
         </>
     );
 }
--- components/users/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CenteredContainer } from "@/components/ui/layout/CenteredContainer";
-
-export const Loading = () => {
-    return (
-        <CenteredContainer>
-            <p>Makám na tom...</p>
-        </CenteredContainer>
-    );
-};
