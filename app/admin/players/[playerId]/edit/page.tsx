cdc20bb24f731f1780a4f967769643962b6b2587
František Pác <5476595+fandasson@users.noreply.github.com>
2026-05-17 13:44:27 +0200

    style: add bottom padding

--- app/admin/players/[playerId]/edit/page.tsx
+++ app/admin/players/[playerId]/edit/page.tsx
@@ -50,7 +50,7 @@ export default async function EditPlayerPage({ params }: { params: Promise<{ pla
     }
 
     return (
-        <div className="space-y-8">
+        <div className="space-y-8 pb-8">
             <div className="flex items-center gap-2">
                 <Button variant="ghost" size="icon" asChild className="h-auto">
                     <Link href="/admin/players">
